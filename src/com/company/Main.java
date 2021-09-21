d065756ccc283c4991be2cb7fe336a6654d5b38b
Pestov236 <91116023+Pestov236@users.noreply.github.com>
2021-09-21 10:35:59 +0300

    1 commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int x = 2;
+        int y =5;
+        if(x>y){
+            System.out.println(x);
+        }
+        else{
+            System.out.println(y);
+
+            int x = 20;
+            if(x>22){
+                if(x<22){
+                    System.out.println("it works");
+                }
+                else {
+                    System.out.println("it doesn't work");
+                else
+                    System.out.println("it");
+                    int x = 8;
+                    if(x>22){
+                        System.out.println("hi");
+                    }
+                    else if(x == 22){
+                        System.out.println(x);
+                    }
+                    else if(x>10){
+                        System.out.println(10);
+                    }
+                    else {
+                        System.out.println("it");
+                    }
+        }
+    }
+}
